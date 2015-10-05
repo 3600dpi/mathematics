318bd900d42d3deee7cceeeaca04e67658e4c5c8
3600 <3600dpi@i.ua>
2015-10-05 11:13:49 +0300

    Revert "1"
    This reverts commit c6d254639a225a401a3e515682303f45ab7968e9.

--- ajaxDelete.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$table = $_SESSION['room'];
-$id = $_POST['id'];
-require_once 'db.php';
-$mysqli = db::getObject();
-$mysqli->query("DELETE FROM $table WHERE `id` = '$id'");
--- notUser.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$name = $_SESSION['login'];
-
-require_once 'db.php';
-$mysqli = db::getObject();
-
-$mysqli->query("DELETE FROM online WHERE user='$name'");
